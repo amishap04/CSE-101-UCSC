fa31a130d522cc1799e2eb131b19f1d72ba35f23
Amisha Prasad <aprasa14@ucsc.edu>
2024-03-04 21:21:13 -0800

    added header and test

--- pa7/Dictionary.h
+++ pa7/Dictionary.h
@@ -7,4 +7,131 @@
  * * * ***/
 
 
+#include<iostream>
+#include<string>
 
+#ifndef DICTIONARY_H_INCLUDE_
+#define DICTIONARY_H_INCLUDE_
+
+
+typedef std::string keyType;  
+typedef int         valType;
+
+class Dictionary{
+
+private:
+
+   struct Node{
+      keyType key;
+      valType val;
+      Node* parent;
+      Node* left;
+      Node* right;
+      Node(keyType k, valType v);
+   };
+
+   Node* nil;
+   Node* root;
+   Node* current;
+   int   num_pairs;
+
+
+   void inOrderString(std::string& s, Node* R) const;
+
+
+   void preOrderString(std::string& s, Node* R) const;
+
+
+   void preOrderCopy(Node* R, Node* N);
+
+
+   void postOrderDelete(Node* R);
+
+
+   Node* search(Node* R, keyType k) const;
+
+
+   Node* findMin(Node* R);
+
+
+   Node* findMax(Node* R);
+
+
+   Node* findNext(Node* N);
+
+
+   Node* findPrev(Node* N);
+
+public:
+
+
+   Dictionary();
+
+   Dictionary(const Dictionary& D);
+
+   ~Dictionary();
+
+
+
+   int size() const;
+
+
+   bool contains(keyType k) const;
+
+
+   valType& getValue(keyType k) const;
+
+
+   bool hasCurrent() const;
+
+
+   keyType currentKey() const;
+
+
+   valType& currentVal() const;
+
+
+   void clear();
+
+
+   void setValue(keyType k, valType v);
+
+
+   void remove(keyType k);
+
+
+   void begin();
+
+
+   void end();
+
+
+   void next();
+
+
+   void prev();
+
+
+
+   std::string to_string() const;
+
+
+   std::string pre_string() const;
+
+
+   bool equals(const Dictionary& D) const;
+
+
+
+   friend std::ostream& operator<<( std::ostream& stream, Dictionary& D );
+
+
+   friend bool operator==( const Dictionary& A, const Dictionary& B );
+
+
+   Dictionary& operator=( const Dictionary& D );
+
+};
+
+
+#endif
--- pa7/DictionaryTest.cpp
+++ pa7/DictionaryTest.cpp
@@ -7,3 +7,161 @@
  * * * ***/
 
 
+#include<iostream>
+#include<string>
+#include<stdexcept>
+#include"Dictionary.h"
+
+using namespace std;
+
+int main(){
+
+   string s;
+   int x;
+   string S[] =   {
+                     "plaices",
+                     "ambusher",
+                     "crosby",
+                     "wattles",
+                     "pardoner",
+                     "pythons",
+                     "second",
+                     "forms",
+                     "impales",
+                     "pic",
+                     "verticals",
+                     "recriminator",
+                     "depressants",
+                     "cul",
+                     "potables",
+                     "norm",
+                     "reinsurer",
+                     "deschooler",
+                     "recoining",
+                     "dissocialised",
+                     "cohabit",
+                     "hemiolia",
+                     "cantling",
+                     "glamorized",
+                     "millesimal",
+                     "glagolitic"
+                  };
+   string T[] =   {
+                     "second",
+                     "forms",
+                     "impales",
+                     "pic",
+                     "verticals",
+                     "recriminator",
+                     "depressants",
+                     "cul",
+                     "potables",
+                     "norm",
+                     "reinsurer",
+                     "deschooler",
+                     "recoining",      
+                  };
+
+   Dictionary A;
+   Dictionary B;
+
+   cout << endl;
+
+   for(int i=0; i<26; i++){
+      A.setValue(S[i], i+1);
+   }
+   
+   B = A;
+
+   cout << "A.size() = " << A.size() << endl  << A << endl;
+   cout << "B.size() = " << B.size() << endl  << B << endl;
+
+   B.setValue("deschooler", 101);
+   B.setValue("reinsurer", 102);
+   B.setValue("glagolitic", 103);
+   B.setValue("cul", 104);
+   B.setValue("ambusher", 105);
+
+   Dictionary C = B;
+
+   cout << "B.size() = " << B.size() << endl  << B << endl;
+   cout << "C.size() = " << C.size() << endl  << C << endl;
+
+   cout << "A==B is " << (A==B?"true":"false") << endl;
+   cout << "B==C is " << (B==C?"true":"false") << endl;
+   cout << "C==A is " << (C==A?"true":"false") << endl << endl;
+
+   cout << A.getValue("hemiolia") << endl;
+   A.getValue("hemiolia") *= 10; 
+   cout << A.getValue("hemiolia") << endl << endl;
+
+   cout << "A.size() = " << A.size() << endl  << A << endl;
+   cout << A.pre_string() << endl;
+
+   for(int i=0; i<13; i++){
+      cout << "removing " << T[i] << " from Dictionary A" << endl; 
+      A.remove(T[i]);
+   }
+
+   cout << "after remove loop" << endl;
+
+   cout << "A.size() = " << A.size() << endl  << A << endl;
+   cout << A.pre_string() << endl;
+
+   for(A.begin(); A.hasCurrent(); A.next()){
+      s = A.currentKey();
+      x = A.currentVal();
+      cout << "("+s+", " << x << ") ";
+   }
+   cout << endl << endl;
+
+   for(A.end(); A.hasCurrent(); A.prev()){
+      s = A.currentKey();
+      x = A.currentVal();
+      cout << "("+s+", " << x << ") ";
+   }
+   cout << endl << endl;
+   
+   try{
+      A.getValue("blah");
+   }catch(logic_error& e){
+      cout << e.what() << endl;
+      cout << "   continuing without interruption" << endl;
+   }
+   try{
+      A.remove("blah");
+   }catch(logic_error& e){
+      cout << e.what() << endl;
+      cout << "   continuing without interruption" << endl;
+   }
+   try{
+      A.currentKey();
+   }catch(logic_error& e){
+      cout << e.what() << endl;
+      cout << "   continuing without interruption" << endl;
+   }
+   try{
+      A.currentVal();
+   }catch(logic_error& e){
+      cout << e.what() << endl;
+      cout << "   continuing without interruption" << endl;
+   }  
+   try{
+      A.next();
+   }catch(logic_error& e){
+      cout << e.what() << endl;
+      cout << "   continuing without interruption" << endl;
+   }
+   try{
+      A.prev();
+   }catch(logic_error& e){
+      cout << e.what() << endl;
+      cout << "   continuing without interruption" << endl;
+   }
+
+   cout << endl;  
+
+   return( EXIT_SUCCESS );
+}
+
+
