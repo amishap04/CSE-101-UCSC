ce41a73e70f95f7f45d8420519a5f13820f824d8
Amisha Prasad <aprasa14@ucsc.edu>
2024-03-06 21:47:05 -0800

    implemented order

--- pa7/Order.cpp
+++ pa7/Order.cpp
@@ -7,4 +7,44 @@
  * * * ***/
 
 
+#include "Dictionary.h"
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 3) {
+        cerr << "Usage: " << argv[0] << " <input file>" << endl;
+        return EXIT_FAILURE;
+    }
+
+    ifstream infile(argv[1]);
+    if (!infile.is_open()) {
+        cerr << "Could not open the file " << argv[1] << endl;
+        return EXIT_FAILURE;
+    }
+    ofstream outfile;
+    outfile.open(argv[2], fstream::out);
+
+
+    Dictionary D;
+    string k = "";
+    int v = 1;
+    while (getline(infile, k)) {
+        D.setValue(k, v);
+        v++;
+    }
+
+    outfile << D.to_string() << endl;
+    outfile << D.pre_string() << endl;
+
+    D.clear();
+
+    infile.close();
+    outfile.close();
+    return EXIT_SUCCESS;
+}
+
+
 
